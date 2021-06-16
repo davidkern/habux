d3a29a24dfea8b744f8fd01bc44e72febde384ef
David Kern <david@mju.io>
2021-06-16 15:13:41 -0700

    implement viewmodel and change tick to handler approach

--- app/src/main/java/io/mju/habux/MainActivity.kt
+++ app/src/main/java/io/mju/habux/MainActivity.kt
@@ -3,6 +3,9 @@ package io.mju.habux
 import android.content.Context
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
 import android.view.Menu
 import android.widget.TextView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
@@ -16,16 +19,14 @@ import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.lifecycle.ViewModelProvider
 import com.android.volley.Request
 import com.android.volley.toolbox.JsonObjectRequest
 
-val habctl_url = "http://10.42.0.1:8080"
+const val HABCTL_URL = "http://10.42.0.1:8080"
+const val REFRESH_INTERVAL_SECONDS = 5
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var textSolarPower: TextView
-    private lateinit var textLilPower: TextView
-    private lateinit var textBigPower: TextView
-    private lateinit var textBatteryVoltage: TextView
     private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -49,19 +50,18 @@ class MainActivity : AppCompatActivity() {
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
 
-        textSolarPower = findViewById(R.id.textSolarPower)
-        textLilPower = findViewById(R.id.textLilPower)
-        textBigPower = findViewById(R.id.textBigPower)
-        textBatteryVoltage = findViewById(R.id.textBatteryVoltage)
+        val viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
 
-        object : CountDownTimer(Long.MAX_VALUE, 5000) {
-            override fun onTick(millisUntilFinished: Long) {
-                requestTelemetry()
-            }
+        val handler = Handler(Looper.getMainLooper())
+        val tick = object: Runnable {
+            override fun run() {
+                Log.d("Handlers", "tick")
+                viewModel.requestTelemetry(this@MainActivity)
 
-            override fun onFinish() {
+                handler.postDelayed(this, REFRESH_INTERVAL_SECONDS.toLong() * 1000)
             }
-        }.start()
+        }
+        handler.post(tick)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -74,45 +74,4 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
-
-    private fun requestTelemetry() =
-            Sys.getInstance(this).networkQueue.add(JsonObjectRequest(
-                    Request.Method.GET,
-                    habctl_url + "/api",
-                    null,
-                    { response ->
-                        val mpptList = response.getJSONArray("mppt")
-                        var batteryVoltage = 0.0
-                        var solarPower = 0
-
-                        for (i in 0 until mpptList.length()) {
-                            val mppt = mpptList.getJSONObject(i)
-                            val name = mppt.getString("name")
-                            val telemetry = mppt.getJSONObject("telemetry")
-                            val panelPower = telemetry.getInt("panel_power")
-
-                            // solar power is sum of all panels
-                            solarPower += panelPower
-                            // battery voltage is average of both controllers
-                            batteryVoltage = batteryVoltage + telemetry.getDouble("battery_voltage")
-
-                            if (name == "lil") {
-                                textLilPower.text = panelPower.toString() + " W"
-                            } else if (name == "big") {
-                                textBigPower.text = panelPower.toString() + " W"
-                            }
-                        }
-
-                        // average the voltages
-                        batteryVoltage = batteryVoltage / mpptList.length()
-
-                        // set cummulative values
-                        textBatteryVoltage.text = "%.2f".format(batteryVoltage) + " V";
-                        textSolarPower.text = solarPower.toString() + " W";
-                    },
-                    { error ->
-                        // TODO: Handle error
-                    }
-            ))
-
 }
---
+++ app/src/main/java/io/mju/habux/MainViewModel.kt
@@ -0,0 +1,88 @@
+package io.mju.habux
+
+import android.content.Context
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.android.volley.Request
+import com.android.volley.toolbox.JsonObjectRequest
+
+class MainViewModel : ViewModel() {
+    // Errors
+    private val _secondsSinceLastUpdate = MutableLiveData<Int>()
+    private val _telemetryErrorMessage = MutableLiveData<String>()
+
+    val timeSinceLastUpdate: LiveData<Int>
+        get() = _secondsSinceLastUpdate
+
+    val telemetryErrorMessage: LiveData<String>
+        get() = _telemetryErrorMessage
+
+    // MPPT data
+    private val _solarPower = MutableLiveData<Int>()
+    private val _lilPower = MutableLiveData<Int>()
+    private val _bigPower = MutableLiveData<Int>()
+    private val _batteryVoltage = MutableLiveData<Double>()
+
+    val solarPower: LiveData<Int>
+        get() = _solarPower
+
+    val lilPower: LiveData<Int>
+        get() = _lilPower
+
+    val bigPower: LiveData<Int>
+        get() = _bigPower
+
+    val batteryVoltage: LiveData<Double>
+        get() = _batteryVoltage
+
+    fun requestTelemetry(context: Context) {
+        Sys.getInstance(context).networkQueue.add(JsonObjectRequest(
+                Request.Method.GET,
+                HABCTL_URL + "/api",
+                null,
+                { response ->
+                    val mpptList = response.getJSONArray("mppt")
+                    var batteryVoltage = 0.0
+                    var solarPower = 0
+
+                    // process mppt devices
+                    for (i in 0 until mpptList.length()) {
+                        val mppt = mpptList.getJSONObject(i)
+                        val name = mppt.getString("name")
+                        val telemetry = mppt.getJSONObject("telemetry")
+                        val panelPower = telemetry.getInt("panel_power")
+
+                        // solar power is sum of all panels
+                        solarPower += panelPower
+                        // battery voltage is average of both controllers
+                        batteryVoltage = batteryVoltage + telemetry.getDouble("battery_voltage")
+
+                        if (name == "lil") {
+                            this._lilPower.value = panelPower;
+                        } else if (name == "big") {
+                            this._bigPower.value = panelPower;
+                        }
+                    }
+
+                    // save accumulated values
+                    this._batteryVoltage.value = batteryVoltage / mpptList.length()
+                    this._solarPower.value = solarPower;
+
+                    // clear errors
+                    this._secondsSinceLastUpdate.value = null;
+                    this._telemetryErrorMessage.value = null;
+                },
+                { error ->
+                    val time = this._secondsSinceLastUpdate.value
+                    if (time != null) {
+                        this._secondsSinceLastUpdate.value = time + REFRESH_INTERVAL_SECONDS;
+                    } else {
+                        this._secondsSinceLastUpdate.value = REFRESH_INTERVAL_SECONDS;
+                    }
+
+                    this._telemetryErrorMessage.value = error.message
+                }
+        ))
+    }
+}
